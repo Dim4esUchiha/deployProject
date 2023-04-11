5193e7dd8781d55e2fe382af53602585b42497f1
KEKWait <dimas_orlov99@mail.ru>
2023-04-11 09:51:16 +0300

    Initial commit

---
+++ springapp/src/main/java/org/dim4es/springapp/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package org.dim4es.springapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
